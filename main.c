1d6eb2941dbdb4990c5942c0345a24f3da6746c6
Eduardo Fantin <edufantinlemes@gmail.com>
2026-03-17 18:30:24 -0300

    organizando o main.c e comentando

--- main.c
+++ main.c
@@ -4,24 +4,19 @@
 #include <stdlib.h>
 
 int main(){
-    srand(time(0));
+    srand(time(0)); //iniciar a seed do random com base no tempo 0 (1 de janeiro de 1970 00:00)
     int tamanhoDaSenha = 0;
-
     tamanhoDaSenha = perguntarSenha();
-    char *arraySenha = alocacaoDeMemoriaSegura(tamanhoDaSenha);
+    char *arraySenha = alocacaoDeMemoriaSegura(tamanhoDaSenha); //malloc
 
     printf("Gerando a senha... aguarde...\n");
-
     clock_t tempo_inicio = clock();
-
     preencherArrayComNumELetras(arraySenha, tamanhoDaSenha);
-
     clock_t tempo_fim = clock();
-
     double tempo_gasto = (double)(tempo_fim - tempo_inicio) / CLOCKS_PER_SEC;
 
     mensagemFinal(arraySenha,tempo_gasto);
 
-    free(arraySenha);
+    free(arraySenha); //liberar a memoria
     return EXIT_SUCCESS;
 }
